1805d54fbe84d1321999901b7d1cec22106290b6
Adrian Unger <dev@staydecent.ca>
2018-07-03 22:03:14 -0700

    Add closure compiler to prod build.

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,7 @@
 // Rollup plugins.
 import replace from 'rollup-plugin-replace'
 import uglify from 'rollup-plugin-uglify'
+import compiler from '@ampproject/rollup-plugin-closure-compiler'
 
 // Import the development configuration.
 import config from './dev'
@@ -8,6 +9,7 @@ import config from './dev'
 // Inject the production settings.
 config.output.file = 'public/bundle.js'
 config.plugins[3] = replace({ 'process.env.NODE_ENV': JSON.stringify('production') })
+config.plugins.push(compiler())
 config.plugins.push(uglify())
 
 export default config
