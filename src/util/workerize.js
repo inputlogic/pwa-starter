4ffc43ab276d091a7c516295ad8324bf7b9323d2
Adrian Unger <dev@staydecent.ca>
2018-07-10 10:21:09 -0700

    Add workerize comment.

--- src/util/workerize.js
+++ src/util/workerize.js
@@ -1,3 +1,17 @@
+// // Build a worker from an anonymous function body
+// var blobURL = URL.createObjectURL( new Blob([ '(',
+
+// function(){
+//     //Long-running work here
+// }.toString(),
+
+// ')()' ], { type: 'application/javascript' } ) ),
+
+// worker = new Worker( blobURL );
+
+// // Won't be needing this anymore
+// URL.revokeObjectURL( blobURL );
+
 export default function workerize (name, ...args) {
   const worker = new window.Worker(`workers/${name}.js`)
   const promise = new Promise((resolve, reject) => {
