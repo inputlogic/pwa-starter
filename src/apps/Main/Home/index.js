db733dfebbae3fb4c3918a61b4295e5a0161bebf
Adrian Unger <dev@staydecent.ca>
2018-08-10 15:10:32 -0700

    Add Dropdown.

--- src/apps/Main/Home/index.js
+++ src/apps/Main/Home/index.js
@@ -1,10 +1,18 @@
 import W from 'wasmuth'
 
 import Carousel from '/elements/Carousel'
+import Dropdown from '/elements/Dropdown'
 
 export default () =>
   <div>
     <h1>Home</h1>
+
+    <Dropdown uid='home-example'>
+      <p>Option</p>
+      <p>Pirate</p>
+      <p>Classy Penguin</p>
+    </Dropdown>
+
     <Carousel withDots>
       {W.map((hex) =>
         <img src={`http://www.placehold.it/400x300/${hex}/f44?text=${hex}`} style='width: 100%;' />,
