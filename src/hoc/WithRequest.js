342971620ced27a53cad226f2ed6dded8665a95c
Adrian Unger <dev@staydecent.ca>
2018-08-10 07:10:32 -0700

    Update WithRequest to be more easily extendable.
    - Don't override state of set by superclass
    - Allow request to be initiated by specified data rather than just initial props (in case the extended class relies on other dynamic data to build the request URL)

--- src/hoc/WithRequest.js
+++ src/hoc/WithRequest.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import equal from '/util/equal'
 import makeRequest from '/util/makeRequest'
 import {getState} from '/store'
@@ -7,7 +6,6 @@ import {getState} from '/store'
 const OK_TIME = 30000
 const CACHE = {}
 const cache = (endpoint, result) => {
-  console.log('cache', {endpoint, result})
   CACHE[endpoint] = {result, timestamp: Date.now()}
   console.log(endpoint, result)
 }
@@ -26,7 +24,7 @@ export const clearCache = endpoint => {
 export default class WithRequest extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {isLoading: true, result: null, error: null}
+    this.state = {...(this.state || {}), isLoading: true, result: null, error: null}
     this._existing = null
   }
 
@@ -36,7 +34,6 @@ export default class WithRequest extends React.Component {
     if (token) {
       headers.Authorization = `Token ${token}`
     }
-    console.log('_performRequest', {endpoint, parse, headers})
     const {xhr, promise} = makeRequest({endpoint, headers})
 
     this._existing = xhr
@@ -47,14 +44,16 @@ export default class WithRequest extends React.Component {
       .catch(error => console.log('_performRequest', {error}) || this.setState({error, isLoading: false}))
   }
 
-  _loadResult () {
+  _loadResult (props) {
+    if (!props.request || !props.request.endpoint) {
+      return
+    }
     if (this._existing && !this.state.error) {
       this._existing.abort()
       this._existing = null
     }
 
-    const {endpoint, parse} = this.props.request
-    console.log('_loadResult', {endpoint, parse})
+    const {endpoint, parse} = props.request
     if (validCache(endpoint)) {
       this.setState({result: CACHE[endpoint].result, isLoading: false})
     } else {
@@ -63,27 +62,27 @@ export default class WithRequest extends React.Component {
   }
 
   componentDidMount () {
-    this._loadResult()
+    this._loadResult(this.props)
   }
 
   shouldComponentUpdate (nextProps, nextState) {
-    if (nextProps.request.endpoint !== this.props.request.endpoint) {
+    const nextEnpoint = (nextProps.request || {}).endpoint
+    const currEnpoint = (this.props.request || {}).endpoint
+    if (currEnpoint !== nextEnpoint) {
       return true
     }
     return !equal(nextState, this.state)
   }
 
-  componentDidUpdate () {
+  componentDidUpdate (prevProps, prevState, snapshot) {
     if (!this._existing) return
-    if (this.props.request.endpoint !== this._existing._endpoint) {
-      console.log('_existing', this._existing._endpoint)
-      this._loadResult()
+    if ((this.props.request || {}).endpoint !== this._existing._endpoint) {
+      this._loadResult(this.props)
     }
   }
 
   render () {
-    const children = this.children || this.props.children
-    const child = children[0]
+    const child = this.props.children || this.children[0]
     if (!child || typeof child !== 'function') {
       throw new Error('WithRequest requires a function as its only child')
     }
