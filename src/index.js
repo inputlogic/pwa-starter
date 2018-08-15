ea5ac7de9ad733a00ba57af40abc6fa593100390
Adrian Unger <dev@staydecent.ca>
2018-08-15 11:20:24 -0700

    WIP docs.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,52 @@
 import Preact from 'preact'
 
+// PWA-Starter is a project skeleton for building Single Page Apps and/or
+// Progressive Web Apps.
+
+// The key technology to be familiar with is React. This skeleton embraces
+// component based architecture, and tries to introduce very few libraries
+// where an API needs to be learned or memorized. To facilitate this, we
+// embrace Higher Order Components (HoC) and renderProps. You should be familiar
+// with both terms, along with General React and JSX knowledge.
+// (@TODO: Link to resources)
+
+// Focus
+// -----
+
+// - Small bundle size
+// - Efficient networking
+// - Speedy development
+// - *Simplicity*
+
+// Simplicity
+// ----------
+
+// Simplicity is **always** encouraged. Generally, this means Components
+// that do one thing. A real world example of this: A former iteration of
+// this starter had a `WithRequests` utility function that let you define
+// any number of requests and bound the results to a child Component.
+
+// In PWA-Starter there is a `WithRequest` HoC, that only manages one request.
+// As your Components should be as simple as possible, a Component should
+// not manage more than one resource.
+
+// Another example, is embracing HoC to be used within your JSX, rather
+// than utility functions:
+
+// This:
+
+// `<WithRequest request={{...}}>...</WithRequest>``
+
+// Rather than this:
+
+// `withRequest({...})(Child)`
+
+// Entry Point and Root Files
+// --------------------------
+
+// `src/index.js` contains your top-most Component and is the entry point
+// for your entire app.
+
 import Header from '/elements/Header'
 import Notification from '/elements/Notification'
 import NotFound from '/elements/NotFound'
@@ -15,6 +62,7 @@ import routes from '/routes'
 import '/styles/variables.less'
 import '/styles/base.less'
 
+// Helmet is fun for SEO PORPOISES
 export const MainApp = () =>
   <div className='main-app-container' >
     <Helmet
