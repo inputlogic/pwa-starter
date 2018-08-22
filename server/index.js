0990725d4e10ec6004c4095e4f7c29067dcb99b1
Adrian Unger <dev@staydecent.ca>
2018-08-22 10:34:23 -0700

    Autoreload browser on changes!

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ const sirv = require('sirv')
 const polka = require('polka')
 const compress = require('compression')()
 
+const DEV = process.env.NODE_ENV !== 'production'
 const port = process.env.PORT || 5000
 
 const assets = sirv('public', {
-  maxAge: 31536000, // 1Y
+  maxAge: !DEV ? 31536000 : 0, // 1Y
   immutable: false
 })
+
+const reloadScript = DEV
+  ? `<script src="${process.env.BROWSER_REFRESH_URL}"></script>`
+  : ''
 
 const ssr = (req, res, next) => {
   console.log('ssr', req.url)
@@ -28,6 +33,7 @@ const ssr = (req, res, next) => {
           <div class='main-app-container'>${html}</div>
           <script>window.__initial_store__ = ${JSON.stringify(state)};</script>
           <script src="./bundle.js"></script>
+          ${reloadScript}
         </body>
       </html>`)
     })
@@ -37,5 +43,8 @@ polka()
   .use(compress, assets, ssr)
   .listen(port)
   .then(() => {
+    if (DEV & process.send) {
+      process.send({event: 'online', url: `http://localhost:${port}/`})
+    }
     console.log(`> Ready on http://localhost:${port}`)
   })
