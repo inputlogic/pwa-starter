135ab8bb3bafb23c3e7997ed0de2ec32e7f6bc4c
Adrian Unger <dev@staydecent.ca>
2019-02-08 13:31:06 -0800

    Disable SSR for now. Prep for netlify.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-import renderReact from './renderReact'
-
 const sirv = require('sirv')
 const polka = require('polka')
 const compress = require('compression')()
@@ -13,25 +11,22 @@ const assets = sirv('public', {
 })
 
 const ssr = (req, res, next) => {
-  console.log('ssr', req.url)
-  renderReact(req.url)
-    .then(({ html, head, state }) => {
-      res.end(`<!doctype html>
-      <html lang="en">
-        <head>
-          <base href="/">
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-          <link rel="stylesheet" href="./bundle.css" />
-          ${head}
-        </head>
-        <body>
-          <div class='main-app-container'>${html}</div>
-          <script>window.__initial_store__ = ${JSON.stringify(state)};</script>
-          <script src="./bundle.js"></script>
-        </body>
-      </html>`)
-    })
+  // renderReact(req.url)
+  //   .then(({ html, head, state }) => {
+  res.end(`<!doctype html>
+    <html lang="en">
+      <head>
+        <base href="/">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" href="./bundle.css" />
+      </head>
+      <body>
+        <div class='main-app-container'>Loading...</div>
+        <script>window.__initial_store__ = "{}";</script>
+        <script src="./bundle.js"></script>
+      </body>
+    </html>`)
 }
 
 polka()
