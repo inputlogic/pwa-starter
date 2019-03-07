fb3c341df2a7d05b6654e77acc219f0de37ae920
Adrian Unger <dev@staydecent.ca>
2019-03-07 12:48:04 -0800

    Update login example to be more full-featured.
    Also, update deps.

--- src/apps/main/login/index.js
+++ src/apps/main/login/index.js
@@ -1,18 +1,33 @@
-import Form from '@app-elements/form'
+import Form, { ErrorOrHint } from '@app-elements/form'
 import { routeTo } from '@app-elements/router'
 
 const TextInput = ({ ...props }) =>
   <input type='text' {...props} />
 
-const handleSubmit = (form) => {
-  routeTo('/', true)
+const formProps = {
+  name: 'Login',
+  validations: {
+    password: (val = '') =>
+      val.length < 6 && 'Password must be greater than 6 characters!'
+  },
+  onSubmit: ({ hasError, errors, data, done }) => {
+    console.log('onSubmit', { hasError, errors, data })
+    if (!hasError) {
+      routeTo('/', true)
+      done() // clear form data and errors
+    } else {
+      // tell the form we are no longer submitting,
+      // but keep the errors, and reset password value.
+      done(errors, { ...data, password: null })
+    }
+  }
 }
 
 const Login = () =>
-  <Form name='Login' onSubmit={handleSubmit}>
-    cool
+  <Form {...formProps}>
     <TextInput required name='email' placeholder='Your Email' />
     <TextInput required type='password' name='password' placeholder='Your Password' />
+    <ErrorOrHint name='password' />
     <button type='submit'>Login</button>
   </Form>
 
