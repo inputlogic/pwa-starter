c2d7626814e65e6d0ecd3f274535765449c785e3
Adrian Unger <dev@staydecent.ca>
2019-04-18 09:27:34 -0700

    Finish init-modal script.

--- scripts/init-modal.js
+++ scripts/init-modal.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-// const { resolve } = require('path')
-// const { exec } = require('child_process')
+const { resolve } = require('path')
+const { writeFile, stat } = require('fs')
+
+const DIR = 'src/modals'
 
 // $ npm run init-modal MyModal
 if (require.main === module) {
@@ -9,13 +11,39 @@ if (require.main === module) {
     console.log('Please provide a name for your new Modal.')
     process.exit(1)
   }
+
   let name = process.argv[process.argv.length - 1]
   if (!name.toLowerCase().includes('modal')) {
     name = `${name}Modal`
   }
+
   if (!/^[$A-Z_][0-9A-Z_$]*$/i.test(name)) {
     console.log('Please provide a proper identifier for your Modal.')
     process.exit(1)
   }
-  console.log({ name })
+
+  const file = resolve(DIR, `${name}.js`)
+  const content = `import Modal from '@app-elements/modal'
+
+export function ${name} () {
+  return (
+    <Modal>
+      <h1>${name}</h1>
+    </Modal>
+  )
+}
+
+export default ${name}`
+
+  stat(file, (err, res) => {
+    if (err == null) {
+      console.log(`File exists: ${file}`)
+      process.exit(1)
+    } else if (err.code === 'ENOENT') {
+      writeFile(file, content + '\n', (err) => {
+        if (err) throw err
+        console.log(`Created ${DIR}/${name}.js`)
+      })
+    }
+  })
 }
