a170a5acfa81e7afd936cc03d839e2e3381c0f7d
Adrian Unger <dev@staydecent.ca>
2019-10-30 09:58:08 -0700

    Add comment on nested components and inlines for forms.

--- src/apps/admin/index.js
+++ src/apps/admin/index.js
@@ -26,6 +26,20 @@ const Delete = () => null
 // Interface for creating custom Actions to be used in the admin.
 const Action = () => null
 
+// Rendering custom components can be done by nesting them in any
+// of the above components. Interacting with data could rely on
+// global atom/redux state or React Context, etc.
+// We could also pass down props by default:
+const MyCustomInline = ({ instance, form, preProcess }) => {
+  // preProcess could accept a function that's called when the form
+  // is submitted, so we have a chance to normalize/mutate the data.
+  return (
+    <div>
+      We could show a related object model to the current instance here.
+    </div>
+  )
+}
+
 const getUser = async (token) => {
   const res = await fetch(url('api.me'), {
     headers: { Authorization: `Token ${token}` }
@@ -49,7 +63,9 @@ export default function AdminApp () {
       <Resource name='User' path='users'>
         <Create />
         <Read fields={['id', 'email', 'createdAt']} />
-        <Update />
+        <Update>
+          <MyCustomInline />
+        </Update>
         <Delete auth={isAdmin} />
         <Action label='Mark selected users as active' action={(selected) => {
           // We want to send a PATCH to API/users with data:
