c9a889659dc5e675ce1017f9d4e01c90c53b29ab
Adrian Unger <dev@staydecent.ca>
2020-03-31 17:19:19 -0700

    WIP cache busting bundle files.

--- config/prod.js
+++ config/prod.js
@@ -3,9 +3,11 @@ import minify from 'rollup-plugin-babel-minify'
 import baseConfig from './base'
 
 const prodConfig = Object.assign({}, baseConfig)
+const timestamp = (new Date()).getTime()
 
-prodConfig.output = prodConfig.output.map(cfg => ({
+prodConfig.output = prodConfig.output.map(cfg => console.log(cfg) || ({
   ...cfg,
+  entryFileNames: `${timestamp}-[name].js`,
   sourcemap: false
 }))
 
@@ -16,4 +18,24 @@ prodConfig.plugins.push(minify({
   keepClassName: true
 }))
 
+prodConfig.plugins.push(cacheBuster({
+  count: prodConfig.output.length,
+  uid: timestamp
+}))
+
 export default prodConfig
+
+function cacheBuster ({ count, uid }) {
+  let c = 0
+  return {
+    name: 'cache-buster',
+    writeBundle (opts) {
+      c++
+      if (c === count) {
+        const filename = Object.keys(opts)[0]
+        console.log('!!! REPLACE', filename)
+      }
+      return null
+    }
+  }
+}
