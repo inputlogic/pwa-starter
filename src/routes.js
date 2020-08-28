c30d6e0da65f2b3dca1324b88385d97c036881b5
Adrian Unger <dev@staydecent.ca>
2020-08-28 10:58:58 -0700

    Add pathReducer to store.
    Closes #86

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,8 @@ export const routes = [
     routes: {
       login: 'https://www.mocky.io/v2/5d28e8362c000068003edca8',
       signup: 'https://www.mocky.io/v2/5d28efd02c0000cd2f3edcc9',
+      // If you want to test error states, use the below mock endpoint.
+      // signup: 'https://run.mocky.io/v3/ecf1c57d-6ab1-4b50-b17b-7646ade58435',
       forgotPassword: 'https://www.mocky.io/v2/5d28e8362c000068003edca8',
       resetPassword: 'https://www.mocky.io/v2/5e98de313500004d00c48670',
       users: 'https://jsonplaceholder.typicode.com/users',
