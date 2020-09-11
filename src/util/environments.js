b4cc81c10370c98a44f1e8b55ffdaef0209c438a
Adrian Unger <dev@staydecent.ca>
2020-09-11 10:30:01 -0700

    Update environments to handle regexp.
    Closes #91

--- src/util/environments.js
+++ src/util/environments.js
@@ -1,26 +1,32 @@
 const environments = {
   test: 'localhost:5001',
-  development: [
-    'localhost:3000'
-  ],
-  staging: ['pwa-starter-html-dev.herokuapp.com'],
-  production: ['pwa-starter-html-prod.herokuapp.com', 'pwa-starter.co', 'www.pwa-starter.co']
+  development: /^localhost:\d{4}$/,
+  staging: /netlify\.app$/,
+  production: ['pwa-starter.com', 'www.pwa-starter.com']
 }
 
 export const environment = (() => {
-  try {
-    const host = window.location.host
-    const current = W.find(
-      (env) => W.toType(environments[env]) === 'array'
-        ? W.some(v => v === host, environments[env])
-        : environments[env] === host,
-      Object.keys(environments)
-    )
-    if (!current) {
-      throw new Error('No environment matching current url')
-    }
-    return current
-  } catch (_) {
-    return '__ENV__' || 'development'
+  const host = window.location.host
+  const current = W.find(
+    (env) => W.toType(environments[env]) === 'array'
+      ? W.some(isMatch(host), environments[env])
+      : isMatch(host)(environments[env]),
+    Object.keys(environments)
+  )
+  if (!current) {
+    throw new Error('No environment matching current url: ' + host)
   }
+  return current
 })()
+
+function isMatch (source) {
+  return (input) => {
+    const type = W.toType(input)
+    if (type === 'string') {
+      return input === source
+    } else if (type === 'regexp') {
+      return input.test(source)
+    }
+    return false
+  }
+}
